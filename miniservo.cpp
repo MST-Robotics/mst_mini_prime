0fc38d6d5aa4f809978724c06a60a56632b2ab85
daniel peterson <jdp6q5@mst.edu>
2011-11-12 11:29:35 -0600

    -added support for full Pololu protocol to miniservo -fixed syntax errors in all files -added some feedback support to serialDriver -started to create a centering routine for the servos. Need to finish still.

--- miniservo.cpp
+++ miniservo.cpp
@@ -7,14 +7,90 @@
 ***********************************************************************/
 #include "miniservo.h"
 
-/***********************************************************************
-* Function Name: setPod
-* Purpose: sets the parameters of a pod and pushes them out the serial port
-* Arguments: po - the pod to set the speed of; top - set the steering 
-* 	motor or the driving motor; speed - the value to push out to the motor
-* Returns: nothing
-***********************************************************************/
-void setPod(int po, bool top, char speed)
+using namespace std;
+
+int miniservo::setPod(int po, bool top, char speed)
 {
+	if (po < 0 || po > 3) return -1; //let the caller know that the pod is out of range
+	//now we can start setting parameters
+	if (top) pod[po -1].angle = speed;
+	else pod[po - 1].velocity = speed;
 	
+	//now that the new parameters are stored, its time to update the servos
+	// the first byte contains the most significat bit. The next byte contains
+	//the lower 7 bits. This is because the Pololu protocal dicatates that
+	//the first bit of a character must be 0
+	char temp[2];
+	
+	
+}
+
+void miniservo::setParam(char servo, char command)
+{
+	char *temp[5];
+	*temp[0] = 0x80; //command start byte (see docs)
+	*temp[1] = 0x01; //device id. We only have one, so the ID is 1
+	*temp[2] = 0x00; //this is command 0 we are sending
+	*temp[3] = servo; //the servo to set the parameters of
+	*temp[4] = command; //the command to send the servo
+	
+	if (sio.isOpen()) sio.writeSerial(temp, 5);
+	else cout << "Serial port closed. Can't write!" << endl;
+}
+
+void miniservo::setSpeed(char servo, char speed)
+{
+	char *temp[5];
+	*temp[0] = 0x80; //command start byte (see docs)
+	*temp[1] = 0x01; //device id. We only have one, so the ID is 1
+	*temp[2] = 0x01; //this is command 1 we are sending
+	*temp[3] = servo; //the servo to set the parameters of
+	*temp[4] = speed; //the speed to rotate at 
+	
+	if (sio.isOpen()) sio.writeSerial(temp, 5);
+	else cout << "Serial port closed. Can't write!" << endl;
+}
+
+void miniservo::setPosition(char servo, char pos[])
+{
+	char *temp[6];
+	*temp[0] = 0x80; //command start byte (see docs)
+	*temp[1] = 0x01; //device id. We only have one, so the ID is 1
+	if ((sizeof(pos)/sizeof(char)) > 1 ) *temp[2] = 0x03;
+	else *temp[2] = 0x02; //this is command 1 we are sending
+	*temp[3] = servo; //the servo to set the parameters of
+	*temp[4] = pos[0]; //the position to rotate to. Can be small or big value
+	if ((sizeof(pos)/sizeof(char)) > 1 ) *temp[5] = pos[1];
+	
+	if (sio.isOpen() && (sizeof(pos)/sizeof(char)) > 1) sio.writeSerial(temp, 6);
+	else if (sio.isOpen()) sio.writeSerial(temp, 5);
+	else cout << "Serial port closed. Can't write!" << endl;
+}
+
+void miniservo::setAbsPosition(char servo, char pos[])
+{
+	char *temp[6];
+	*temp[0] = 0x80; //command start byte (see docs)
+	*temp[1] = 0x04; //device id. We only have one, so the ID is 1
+	*temp[2] = 0x01; //this is command 1 we are sending
+	*temp[3] = servo; //the servo to set the parameters of
+	*temp[4] = pos[0]; //the position to rotate to (500 to 5500)
+	*temp[5] = pos[1];
+	
+	if (sio.isOpen()) sio.writeSerial(temp, 6);
+	else cout << "Serial port closed. Can't write!" << endl;
+}
+
+void miniservo::setNeutral(char servo, char pos[])
+{
+	char *temp[6];
+	*temp[0] = 0x80; //command start byte (see docs)
+	*temp[1] = 0x04; //device id. We only have one, so the ID is 1
+	*temp[2] = 0x05; //this is command 1 we are sending
+	*temp[3] = servo; //the servo to set the parameters of
+	*temp[4] = pos[0]; //the position to set as neutral
+	*temp[5] = pos[1];
+	
+	if (sio.isOpen()) sio.writeSerial(temp, 6);
+	else cout << "Serial port closed. Can't write!" << endl;
 }
--- miniservo.h
+++ miniservo.h
@@ -4,34 +4,97 @@
 *
 *
 ***********************************************************************/
+#ifndef MINISERVO_H_
+#define MINISERHO_H_
+
+#include "serialDriver.h"
+#include <iostream>
 
 class miniservo 
 {
+	//holds the information for a wheel pod
 	struct pod
 	{
-		char rot = 0;
-		char drive = 0;
-	}
+		//these two hold the motor numbers
+		char rot;
+		char drive;
+		//these two hold the angle and speed of the pod
+		char velocity;
+		char angle;
+	};
 	
 	public:
 	//variables first
-		char sio[] = ""; //holds the string for the serial controller we are using
-		
-		struct pod p1, p2, p3; //miniprime only has three pods
+		struct pod[3]; //miniprime only has three pods
 		
 		//now functions
-		miniservo () {sio  = "/dev/ttyUSB2"}
+		miniservo () {sio.initializeSerialPort("/dev/ttyUSB2");}
 		
-		miniservo (const char serial)
-		{
-			sio = serial;
-			
-		}
+		miniservo (char *serial) {sio.initializeSerialPort(serial);}
 		
-		//set the speed/position of a pod
-		setPod(int po, bool top, char speed); 
+/***********************************************************************
+* Function Name: setPod
+* Purpose: sets the parameters of a pod and pushes them out the serial port
+* Arguments: po - the pod to set the speed of; top - set the steering 
+* 	motor or the driving motor; speed - the value to push out to the motor
+* Returns: nothing
+***********************************************************************/
+	int setPod(int po, bool top, char speed); 
 	
 	private:
-		setSpeed(char servo, 
 	
-}
+		serialDriver sio; //port to use for serial IO
+/***********************************************************************
+* Function Name: setParam (command 0)
+* Purpose: sets the parameters of a servo 
+* Arguments: servo - the number of the servo to set; command - the hex 
+* 		value of the command to send. See the Pololou manual for more
+* 		information on the Pololou protocol
+*
+* Returns: nothing
+***********************************************************************/
+		void setParam(char servo, char command); 
+		
+/***********************************************************************
+* Function Name: setSpeed (command 1)
+* Purpose: sets the speed at which a servo changes position
+* Arguments: servo - the number of the servo to set; speed - this basically
+* 		determines how long a servo pulse lasts and how many pulses will
+* 		be used to change a position.
+* Returns: nothing
+***********************************************************************/
+		void setSpeed(char servo, char speed); 
+
+/***********************************************************************
+* Function Name: setPosition (command 2 or 3)
+* Purpose: commands the servo to move to a certian position. The caller
+* 		sets the step resolution
+* Arguments: servo - the number of the servo to set; pos - the position
+* 	to set the motor to. If there are two bytes in there, then use
+* 	command 4, else use command 3. The difference is resolution
+* Returns: nothing
+***********************************************************************/
+		void setPosition(char servo, char pos[]); 	
+		
+/***********************************************************************
+* Function Name: setAbsPosition (command 4)
+* Purpose: sets the Absolution position of the servo (no offsets)
+* Arguments: servo - the number of the servo to set; pos - the position
+* 	to command the servo to move to. This ignores any offsets such as 
+* 	the neutral position.
+* Returns: nothing
+***********************************************************************/
+		void setAbsPosition(char servo, char pos[]); 		
+		
+/***********************************************************************
+* Function Name: setNeutral (command 5)
+* Purpose: sets the position where the servo controller considers the
+* 		servo to be at neutral 
+* Arguments: servo - the number of the servo to set; pos - the position
+* 	to set as the servo neutral point. 
+* Returns: nothing
+***********************************************************************/
+		void setNeutral(char servo, char pos[]);
+};
+
+#endif
